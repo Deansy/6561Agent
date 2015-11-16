0c2006eb5d61e6324ac5c3aa5ad976b8117165a0
Cameron Deans <CDeansy@gmail.com>
2015-11-16 11:40:26 +0000

    Now slides left correctly

--- src/Board.java
+++ src/Board.java
@@ -28,57 +28,39 @@ public class Board {
     // Create a thread for each row and have each thread slide only one row
 
 
+
     public void slideLeft() {
+
         // Copy the board, Perform any operations and replace the board afterwards
         String[][] boardCopy = board.clone();
 
-        // We are only shifting the top row currently
-        int y = 0;
-        // This builds the current row, Mainly for debugging purposes
-        String currentRow = boardCopy[0][y] + boardCopy[1][y] +boardCopy[2][y] +boardCopy[3][y];
 
+        for (int y = 0; y < 4; y++) {
 
-        // for each tile from left to right
-        int i = 0;
-        while (i < boardWidth) {
-            // If the tile is 0, we need to shift tiles into it
-            if (boardCopy[i][y].equals("0")) {
-                int j = i;
+            for (int x = 0; x < 4; x++) {
+                if (boardCopy[x][y].equals("0")) {
 
-                // j will loop through all tiles to the right of i and shift them
-                // while we still have tiles to shift
-                while (j < boardWidth) {
-                    // We reached the last tile, Place with a 0, instead of trying to get off the board
-                    if (j+1 == boardWidth) {
-                        boardCopy[j][y] = "0";
-                        currentRow = boardCopy[0][y] + boardCopy[1][y] +boardCopy[2][y] +boardCopy[3][y];
+                    for (int k=x+1; k<4; k++) {
+                        if (boardCopy[k][y].equals("0") == false) {
+                            boardCopy[x][y] = boardCopy[k][y];
+                            boardCopy[k][y] = "0";
+                            break;
+                        }
                     }
-                    else {
-                        // Replace the current cell with the tile to the right
-                        boardCopy[j][y] = boardCopy[j+1][y];
-                        currentRow = boardCopy[0][y] + boardCopy[1][y] +boardCopy[2][y] +boardCopy[3][y];
-                    }
-                    j++;
                 }
-
-            }
-
-            // Without this check it wont shift far enough
-            // With it, It gets stuck here
-            //TODO: I need to a check that ensures all shifting is done and I can get out of this function
-            if (boardCopy[i][y].equals("0") == false) {
-                i++;
             }
         }
 
         board = boardCopy;
+
     }
 
 
-    public void placeTile(TileColor tileColor, int xPos, int yPos) {
+
+    public void placeTile(TileColor tileColor, int xPos, int yPos, String value) {
         if (xPos < boardWidth - 1 || xPos >= 0) {
             if (yPos < boardHeight - 1 || yPos >= 0) {
-                board[xPos][yPos] = "1";
+                board[xPos][yPos] = value;
             }
             else {
                 System.out.println("Error placing tile: 1");
--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,22 @@ public class Main {
 
         Board b = new Board();
 
-        //b.placeTile(Board.TileColor.BLUE, 1, 0);
-        //b.placeTile(Board.TileColor.BLUE, 3, 0);
-        b.placeTile(Board.TileColor.BLUE, 2, 0);
+        b.placeTile(Board.TileColor.BLUE, 2, 0, "1");
+        b.placeTile(Board.TileColor.BLUE, 3, 0, "2");
+
+        b.placeTile(Board.TileColor.BLUE, 1, 1, "2");
+        b.placeTile(Board.TileColor.BLUE, 2, 1, "1");
+
+        b.placeTile(Board.TileColor.BLUE, 2, 2, "2");
+
+        b.placeTile(Board.TileColor.BLUE, 3, 3, "2");
+
 
         b.printBoard();
 
+
         b.slideLeft();
+
 
         b.printBoard();
     }
